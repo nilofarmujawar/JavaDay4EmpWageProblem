e5ffcec191a2eaccda185de2d27eea39548c7e51
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-17 10:31:14 +0530

    Solving using switch case statement.

--- src/com/bridgelab/EmpWage4/EmpWage.java
+++ src/com/bridgelab/EmpWage4/EmpWage.java
@@ -1,18 +1,35 @@
 package com.bridgelab.EmpWage4;
 
 public class EmpWage {
-    public static void main(String[] args) {
-        int isPartTime =2;
-        int isFullTime = 1;
-        int empRatePerHour = 20;
-        int empHrs = 0;
-        int totalWage = 0;
-        double empcheck= Math.floor(Math.random() *10) % 3;
-        if (empcheck == isFullTime)
-            empHrs = 8;
-        else if (empcheck == isPartTime)
-            empHrs = 4;
-        totalWage = empHrs * empRatePerHour;
-        System.out.println("Emp WAge: " + totalWage);
+
+        public static final int isPartTime = 2;
+        public static final int isFullTime = 1;
+        public static final int empRatePerHour = 20;
+
+        public static void main(String[] args) {
+
+            int empHrs = 0;
+            int totalWage = 0;
+
+            double empcheck= Math.floor(Math.random() *10) % 3;
+
+            switch ((int)empcheck){
+
+                case isFullTime:
+                    empHrs=8;
+                    break;
+
+                case isPartTime:
+                    empHrs=4;
+                    break;
+
+                default:
+                    empHrs=0;
+
+            }
+
+            totalWage = empHrs * empRatePerHour;
+
+            System.out.println("Emp WAge: " + totalWage);
+        }
     }
-}
