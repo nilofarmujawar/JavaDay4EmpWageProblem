a05c691cf934d1acf144dc355a0d38c13807adff
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-17 10:07:12 +0530

    calculate daily empwage per hr and full day.

--- src/com/bridgelab/EmpWage4/EmpWage.java
+++ src/com/bridgelab/EmpWage4/EmpWage.java
@@ -3,11 +3,13 @@ package com.bridgelab.EmpWage4;
 public class EmpWage {
     public static void main(String[] args) {
         int isFullTime = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int totalWage = 0;
         double empcheck= Math.floor(Math.random() *10) % 2;
         if (empcheck == isFullTime)
-            System.out.println("Employee is present");
-        else
-            System.out.println("Employee is Absent");
+            empHrs = 8;
+        totalWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp WAge: " + totalWage);
     }
 }
-
