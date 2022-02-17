6f809284d87b4553fb5c85664d682b8290d8ea7f
nilofarmujawar1118 <nilofarmujawar1118@gmail.com>
2022-02-17 10:13:47 +0530

    adding part time empwage.

--- src/com/bridgelab/EmpWage4/EmpWage.java
+++ src/com/bridgelab/EmpWage4/EmpWage.java
@@ -2,14 +2,17 @@ package com.bridgelab.EmpWage4;
 
 public class EmpWage {
     public static void main(String[] args) {
+        int isPartTime =2;
         int isFullTime = 1;
-        int EMP_RATE_PER_HOUR = 20;
+        int empRatePerHour = 20;
         int empHrs = 0;
         int totalWage = 0;
-        double empcheck= Math.floor(Math.random() *10) % 2;
+        double empcheck= Math.floor(Math.random() *10) % 3;
         if (empcheck == isFullTime)
             empHrs = 8;
-        totalWage = empHrs * EMP_RATE_PER_HOUR;
+        else if (empcheck == isPartTime)
+            empHrs = 4;
+        totalWage = empHrs * empRatePerHour;
         System.out.println("Emp WAge: " + totalWage);
     }
 }
